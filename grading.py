67fc04a840de05b825ce01b8ec43549adc30c1a6
Gangadhar P Kulam <56226081+GP-Kulam@users.noreply.github.com>
2020-03-08 21:08:28 +0530

    Add files via upload
    HackerLand University has the following grading policy:
    
    Every student receives a  in the inclusive range from  to .
    Any  less than  is a failing grade.
    Sam is a professor at the university and likes to round each student's  according to these rules:
    
    If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
    If the value of  is less than , no rounding occurs as the result will still be a failing grade.
    For example,  will be rounded to  but  will not be rounded because the rounding would result in a number that is less than .
    
    Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
    
    Function Description
    
    Complete the function gradingStudents in the editor below. It should return an integer array consisting of rounded grades.
    
    gradingStudents has the following parameter(s):
    
    grades: an array of integers representing grades before rounding
    Input Format
    
    The first line contains a single integer, , the number of students.
    Each line  of the  subsequent lines contains a single integer, , denoting student 's grade.
    
    Constraints
    
    Output Format
    
    For each , print the rounded grade on a new line.
    
    Sample Input 0
    
    4
    73
    67
    38
    33
    Sample Output 0
    
    75
    67
    40
    33

---
+++ grading.py
@@ -0,0 +1,22 @@
+def gradingStudents(grades):
+    # Write your code here
+    final=[]
+    for grade in grades:
+        if(grade>36):
+            x=grade%5
+            y=5-x
+            if(y<5):
+                if((grade+y)-grade<3):
+                    grade=grade+y
+                    final.append(grade)
+                else:
+                    final.append(grade)
+            else:
+                final.append(grade)
+        else:
+            final.append(grade)
+    return final
+
+if __name__ == '__main__':
+        grades=[12,43,23,76,50]
+        print(gradingStudents(grades))
